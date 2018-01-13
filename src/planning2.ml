f6cb6013c687fb6af52ac3a542935173331a7665
ARAYE Anthony <anthonynjiva@free.fr>
2018-01-13 01:56:17 +0100

    finish of conversion into map struture (not tested yet)

--- src/planning2.ml
+++ src/planning2.ml
@@ -62,7 +62,7 @@ let rec queue_to_list q =
   MapQueue.bindings q;;
 
 (* continuer update_weight *)
-let update_weight n deb weight q =
+let update_weight n deb weight g q =
   MapQueue.fold
     (
       fun k v acc ->
@@ -82,46 +82,57 @@ let update_weight n deb weight q =
         let (indice,e1,e2,p,total) = a2 in
         if (indice = -1) then acc
         else
-          let tmp = MapQueue.add k v acc in
-          add n edge g p1 p2 total q
-          add n (Graph.edge e1 e2) g (k+weight) (p+weight) (total+weight)
+          if (a1 = [])
+          then
+            let tmp1 = MapQueue.remove k acc in
+            add n (Graph.edge e1 e2) g (k+weight) (p+weight) (total+weight) tmp1
+          else
+            let tmp2 = MapQueue.add k a1 acc in
+            add n (Graph.edge e1 e2) g (k+weight) (p+weight) (total+weight) tmp2
     ) q q
 
-let update q =
-  let rec check elt q =
-    let (n1,edge1,p11,p12) = elt in
-    match q with
-      [] -> 0
-      |(n,edge,p1,p2)::s ->
-        let (e11,e12) = Graph.edge_split edge1 in
-        let (e21,e22) = Graph.edge_split edge in
-        if ((p12 <= p1) || (p11 >= p2))
-        then
-          check elt s
-        else if (e11 = e21 && e12 = e22 && p11 < p2)
-        then
-          p2-p11
-        else
-          check elt s
+let update g q =
+  let check elt q =
+    let (n,e1,e2,p,total) = elt in
+    let k1 = p-(Graph.find_edge (Graph.edge e1 e2) g) in
+    MapQueue.fold
+      (
+        fun k v acc ->
+          let rec aux l acc1 =
+            match l with
+              |[] -> acc1
+              |((n1,e11,e12,p1,total1)::s) ->
+                if (k1 >= p1) then aux s acc1
+                else if ((e1=e11 && e2=e12) || (e1=e12 && e2=e11))
+                then
+                  p1-k1
+                else aux s acc1
+          in
+          if (p <= k) then acc
+          else max (aux v 0) acc
+      ) q 0
   in
-  let rec aux acc q =
-    match q with
-      |[] -> acc
-      |(n,edge,p1,p2)::s ->
-        let weight = check (n,edge,p1,p2) acc in
-        if (weight = 0)
-        then
-          aux (add n edge p1 p2 acc) s
-        else
-          let q2 = update_weight n weight s in
-          aux (add n edge (p1+weight) (p2+weight) acc) q2
-  in
-  aux [] q;;
+  MapQueue.fold
+    (
+      fun k v acc ->
+        let rec aux l acc1 q1 =
+          match l with
+            |[] -> acc1
+            |((n,e1,e2,p,total)::s) ->
+              let weight = check (n,e1,e2,p,total) acc1 in
+              if (weight = 0)
+              then
+                aux s (add n (Graph.edge e1 e2) g k p total acc1) q1
+              else
+                let q2 = update_weight n weight q1 in
+                aux s (add n (Graph.edge e1 e2) g (k+weight) (p+weight) (total+weight) acc1) q2
+
+    ) q MapQueue.empty
 
 let split_queue q nb =
   let rec aux1 i =
     match i with
-      |0 ->[]
+      |0 -> []
       |i -> []::(aux1 (i-1))
   in
   let rec aux2 li elt i =
@@ -130,9 +141,14 @@ let split_queue q nb =
       |i,[] -> raise Empty
       |i,(t::s) -> t::(aux2 s elt (i-1))
   in
-  let rec aux3 q li =
-    match q with
-      |[] -> li
-      |(n,e,p1,p2)::s -> aux3 s (aux2 li (n,e,p1,p2) n)
-  in
-  aux3 q (aux1 nb);;
+  MapQueue.fold
+    (
+      fun k v acc ->
+        let rec aux l acc1 =
+          match l with
+            |[] -> acc1
+            |((n,e1,e2,p,total)::s) ->
+              aux s (aux2 acc1 (n,e1,e2,p,total) n)
+        in
+        aux v acc
+    ) q (aux1 nb);;
