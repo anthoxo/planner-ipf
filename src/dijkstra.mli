965caee15b4945d2cd8ce567a8964f105080b9ad
ARAYE Anthony <anthonynjiva@free.fr>
2018-01-14 03:41:33 +0100

    add commentary into graph.mli and dijkstra.mli

--- src/dijkstra.mli
+++ src/dijkstra.mli
@@ -3,93 +3,102 @@ Ce module représente toutes les fonctions utiles pour
 l'application de l'algorithme de Dijkstra.
 *)
 
-(* ceci représente le type des noeuds du graphe pour Dijkstra *)
+(* [dijkstra] représente le type des noeuds du graphe pour Dijkstra *)
 type dijkstra
 
-(**
+(*
+[empty] représente l'ensemble vide
+
 @requires : rien
-@ensures : ensemble vide
+@ensures : retourne l'ensemble vide
 @raises : rien
 *)
 val empty : dijkstra
 
-(**
-@requires :
-  un élément de type String (représentant le noeud de départ)
-  un graphe
-@ensures :
-  retourne une liste de tuple dont les tuples sont composés :
-    - du noeud
-    - d'un poids représentant la distance entre ce noeud et celui de départ
-    - d'un boolean permettant de savoir si ce noeud a été parcouru ou non
-    - d'un noeud représentant son prédécesseur s'il existe (sinon "")
-  Nous nommerons cette liste de tuple dans la suite
-  la liste représentant la progression dans l'algorithme de Dijkstra.
-@raises : rien
+(*
+[initialisation_dijkstra src g] initialise un ensemble de type dijkstra
+avec [src] comme noeud de départ
 
-Cette fonction initialise la liste de progression.
+@requires :
+  [src] un string
+  [g] une valeur de type graph
+@ensures :
+  retourne [sol] une liste telle que [sol_i] le i-ièmede élément est de
+  de la forme [(n,k,weight,bool)] où
+    - [n] un string représentant le prédécesseur
+    - [k] un string représentant le noeud courant
+    - [weight] un int représentant le poids entre le noeud de départ et celui-ci
+    - [bool] un boolean représentant le fait d'être déjà passé ou non sur le noeud
+@raises : rien
 *)
 val initialisation_dijkstra : string -> Graph.graph -> dijkstra
 
-(**
-@requires :
-  une liste de tuples représentant la progression dans l'algorithme
-  un graphe
-@ensures :
-  retourne la liste de tuple actualisée
-@raises : rien
+(*
+[find_min_dijkstra tab g] détermine le noeud le plus rapidement atteignable
+et indique que le noeud en question a été parcourue
 
-Cette fonction trouve le noeud atteignable le plus rapidement possibe et
-indique que le noeud en question a été parcouru.
-Elle se charge également de trier la liste en faisant en sorte que les éléments
-déjà parcourus se retrouvent en fin de liste.
+@requires :
+  [tab] une valeur de type dijkstra
+  [g] une valeur de type graph
+@ensures :
+  retourne la Map actualisée ainsi que le noeud parcouru
+@raises : rien
 *)
 val find_min_dijkstra : dijkstra -> Graph.graph -> dijkstra * (string * string * int * bool)
 
-(**
+(*
+[maj_distance init last g] actualise les poids de chaque noeud
+
 @requires :
-  une liste de tuples représentant la progression dans l'algorithme
-  un graphe
+  [init] une valeur de type dijkstra
+  [last] un tuple (string * string * int * bool)
+  [g] une valeur de type graph
 @ensures :
   retourne la liste de tuple actualisée
-@raises : rien
+@raises : Not_found s'il ne trouve pas le noeud (donc un chemin non valide)
 
 Cette fonction se charge d'actualiser les distances entre le noeud de départ
 et les différents noeuds atteignables.
 *)
 val maj_distance : dijkstra -> (string * string * int * bool) -> Graph.graph -> dijkstra
 
-(**
-@requires :
-  un noeud de départ
-  un noeud d'arrivée
-  un graphe
-@ensures :
-  une liste de tuples représentant les distances entre un noeud et celui de départ.
-@raises : rien
+(*
+[dijkstra src dest g] applique l'algorithme de Dijkstra grâce aux fonctions
+précédentes
 
-Cette fonction applique l'algorithme de Dijkstra.
+@requires :
+  [src] un string
+  [dest] un string
+  [g] une valeur de type graph
+@ensures :
+  retourne une Map réprésentant la conclusion de l'algorithme de Dijkstra
+@raises : Not_found (par le biais de [maj_distance init last g])
 *)
 val dijkstra : string -> string -> Graph.graph -> dijkstra
 
-(**
+(*
+[distance tab node] renvoie la distance finale
+
 @requires :
-  une liste de tuples représentant la progression dans l'algorithme
-  un noeud d'arrivée
+  [tab] une valeur de type dijkstra
+  [node] un string
 @ensures :
   un entier représentant la distance totale parcourue
-@raises : Empty dans le cas où l'algorithme n'a pas pu proposer de chemin
+@raises : rien
 *)
 val distance : dijkstra -> string -> int
 
-(**
+(*
+[path tab src dest] retourne une liste représentant le chemin entre
+[src] et [dest]
+
 @requires :
-  une liste de tuples représentant la progression dans l'algorithme
-  un noeud de départ
-  un noeud d'arrivée
+  [tab] une valeur de type dijkstra
+  [src] un string
+  [dest] un string
 @ensures :
   une liste de noeuds représentant l'ordre dans laquelle on passe de la source
     à la destination.
-@raises : Empty dans le cas où l'algorithme n'a pas pu proposer de chemin
+@raises : Not_found dans le cas où l'algorithme n'a pas pu proposer de chemin
 *)
 val path : dijkstra -> string -> string -> string list
--- src/graph.mli
+++ src/graph.mli
@@ -1,111 +1,130 @@
-(**
-Ce module représente le graphe en lui-même
-*)
+(* Ce module représente la structure de graphe  *)
 
-(** ceci représente le type des nœuds *)
+(* [node] représente le type des nœuds *)
 type node
 
-(** ceci représente le type des arêtes *)
+(* [edge] représente le type des arêtes *)
 type edge
 
-(* ce type représente le graphe en lui-même *)
+(* [graph] représente le graphe en lui-même *)
 type graph
 
-(**
-@requires : deux chaînes de caractères
-@ensures : une arête
+(*
+[edge src dest] construit une arête à partir de deux chaînes de caractères
+
+@requires : [src] et [dest] des strings
+@ensures : une valeur de type edge
 @raises : rien
 *)
-(** elle construit une arête à partir de deux chaînes de caractères *)
 val edge : string -> string -> edge
 
-(**
-@requires : une arête
-@ensures : deux chaînes de caractères
+(*
+[edge_split (a,b)] renvoie un couple de strings.
+
+@requires : [(a,b)] une valeur de type edge
+@ensures : un couple de strings
 @raises : rien
 *)
 val edge_split : edge -> string * string
 
-(**
+(*
+[empty] représente le graphe vide
+
 @requires : rien
 @ensures : retourne le graphe vide
 @raises : rien
 *)
 val empty : graph
 
-(**
-@requires : un graphe
+(*
+[is_empty g] vérifie si le graph est vide ou non
+
+@requires : [g] est une valeur de type graph
 @ensures : retourne true si le graphe est vide, false sinon
 @raises : rien
 *)
 val is_empty : graph -> bool
 
-(**
-@requires : un graphe
-@ensures : retourne le nombre de nœuds dans ce graphe
+(*
+[cardinal g] retourne le nombre de noeuds dans ce graphe
+@requires : [g] est une valeur de type graph
+@ensures : retourne le nombre de noeuds dans ce graphe
 @raises : rien
 *)
 val cardinal : graph -> int
 
-(**
+(*
+[mem_node n g] vérifie si un noeud [n] se trouve dans un graphe [g]
+
 @requires :
-  un string représentant un nœud
-  un graphe
+  [n] est un string
+  [g] est une valeur de type graph
 @ensures :
-  retourne vrai si le nœud se trouve dans le graphe,
-  retourne faux sinon
+  retourne true si le noeud se trouve dans le graphe,
+  retourne false sinon
 @raises : rien
 *)
 val mem_node : string -> graph -> bool
 
-(**
+(*
+[add_edge (n1,n2) weight g] ajoute une arête [(n1,n2)] dans un graphe [g]
+
 @requires :
-  une clé représentant une arête à ajouter dans un graphe
-  une valeur (entier) représentant l'étiquette d'une arête
-  un graphe
+  [(n1,n2)] une valeur de type edge
+  [weight] un entier représentant l'étiquette de l'arête
+  [g] une valeur de type graph
 @ensures :
   retourne le graphe avec le nouvel arc s'il n'y est pas
-    sinon renvoit le graphe inchangé
-  ajoute les nœuds s'ils ne sont pas dans la liste des nœuds
+  sinon renvoit le graphe inchangé
 @raises : rien
 *)
-(* cette méthode se charge d'ajouter une arête à un graphe *)
 val add_edge : edge -> int -> graph -> graph
 
-(**
+(*
+[add_edge_list li g] ajoute les arêtes de la liste [li]
+
 @requires :
-  une liste de type (string * string * 'a) contenant deux nœuds
-    pour l'arête avec son étiquette
-  un graphe
+  [li] une liste de (string * string * 'a)
+  [g] une valeur de type graph
 @ensures :
   retourne le graphe avec les nouvelles arêtes
-  ajoute les nœuds s'ils ne sont pas dans la liste
 @raises : rien
 *)
 val add_edge_list : (string * string * int) list -> graph -> graph
 
-(**
+(*
+[find_edge (n1,n2) g] retourne l'étiquette d'une arête, -1 sinon
 @requires :
-  un élément de type Edge (string * string)
-  un graphe
+  [(n1,n2)] une valeur de type edge
+  [g] une valeur de type graph
 @ensures :
   retourne l'étiquette de l'arête,
   retourne -1 si l'arête n'est pas dans le graphe
-@raises : Not_found (dans le cas où l'arête n'est pas dans le graphe)
+@raises : rien
 *)
 val find_edge : edge -> graph -> int
 
-(**
+(*
+[fold f g acc] applique la méthode fold du module Map
+
 @requires :
-  une fonction f (key -> 'a -> 'b -> 'b)
-  un graphe
+  [f] une fonction de type (key -> 'a -> 'b -> 'b)
+  [g] une valeur de type graph
+  [acc] un accumulateur de type 'b
 @ensures :
   applique la fonction fold de MapGraph
 @raises : rien
 *)
 val fold : (string -> node -> 'b -> 'b) -> graph -> 'b -> 'b
 
-(**
+(*
+[distance_path li g] calcule la distance parcourue par un chemin [li]
 
+@requires :
+  [li] une liste
+  [g] une valeur de type graph
+@ensures :
+  calcule la distance selon une liste
+@raises : rien
 *)
 val distance_path : string list -> graph -> int
